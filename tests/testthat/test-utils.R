156bea30ac80feb42d2bea23e78d0c387f4d3b2f
Patrice Lecharpentier <patrice.lecharpentier@inrae.fr>
2021-07-30 12:53:05 +0200

    added: SticsOnR

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -3,6 +3,6 @@ context("test-utils.R")
 test_that("SticsRPacks packages returns character vector of package names", {
   out <- SticsRPacks_packages()
   expect_type(out, "character")
-  expect_true(all(c("CroptimizR","SticsRFiles","CroPlotR","SticsRPacks") %in%
+  expect_true(all(c("CroptimizR","SticsRFiles", "SticsOnR", "CroPlotR","SticsRPacks") %in%
                     out))
 })
